ed50e768a174eb5222100f9a016e118a0849f0e9
callison98 <callison4@mail.csuchico.edu>
2020-02-12 11:32:05 -0800

    Added description of each function

--- Inventory.cpp
+++ Inventory.cpp
@@ -4,19 +4,27 @@
 
 using std::string;
 using std::ostream;
-  
-Inventory::Inventory(string name, float price, int count)
+using std::cout;
+// This is the constructor that intializes the name, price, and count  
+Inventory::Inventory(string name, float price, int count) 
 {
   m_name = name;
   m_price = price;
   m_in_stock = count;
 }
-
-void Inventory::sell()
+// This member function will decrement the stock by one. It will not allow you to sell an item if its less than zero
+void Inventory::sell() // 
 {
-  m_in_stock--;
+	if (m_in_stock > 0)
+	{
+		m_in_stock--;
+	}
+	else
+	{
+		cout << "Sorry, that item is out of stock" << "\n";
+	}  
 }
-
+// This overloads the price on the inenvtory item by setting the precision of decimals to 2
 ostream& operator<<(ostream& stream, const Inventory& item)
 {
   stream << item.m_name 
